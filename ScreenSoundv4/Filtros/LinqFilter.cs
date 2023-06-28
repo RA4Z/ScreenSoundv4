d21f116b1c700b1bb23d29e6e9b575c02bf6af35
RA4Z <rraz639@gmail.com>
2023-06-28 09:18:07 -0300

    Mostrando cada um dos gêneros musicais

---
+++ ScreenSoundv4/Filtros/LinqFilter.cs
@@ -0,0 +1,15 @@
+﻿using ScreenSoundv4.Modelos;
+
+namespace ScreenSoundv4.Filtros;
+
+internal class LinqFilter
+{
+    public static void FiltrarTodosOsGenerosMusicais(List<Musica> musicas)
+    {
+        var todosOsGenerosMusicais = musicas.Select(generos => generos.Genero).Distinct().ToList();
+        foreach(var genero in todosOsGenerosMusicais)
+        {
+            Console.WriteLine($"- {genero}");
+        }
+    }
+}
--- ScreenSoundv4/Program.cs
+++ ScreenSoundv4/Program.cs
@@ -1,5 +1,6 @@
 ﻿using ScreenSoundv4.Modelos;
 using System.Text.Json;
+using ScreenSoundv4.Filtros;
 
 using (HttpClient client = new HttpClient())
 {
@@ -8,7 +9,7 @@ using (HttpClient client = new HttpClient())
         string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
         Console.WriteLine(resposta);
         var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta);
-        musicas[0].ExibirDetalhesDaMusica();
+        LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
     }
     catch (Exception ex)
     {
