735188f5cf62b3077c8155813342f9827810755b
RA4Z <rraz639@gmail.com>
2023-06-28 09:23:55 -0300

    Filtrando todos os artistas

---
+++ ScreenSoundv4/Filtros/LinqOrder.cs
@@ -0,0 +1,20 @@
+﻿using ScreenSoundv4.Modelos;
+
+namespace ScreenSoundv4.Filtros
+{
+    internal class LinqOrder
+    {
+        public static void ExibirListaDeArtistasOrdenados(List<Musica> musicas)
+        {
+            var artistasOrdenados = musicas.OrderBy(musica => musica.Artista)
+                .Select(musica => musica.Artista)
+                .Distinct()
+                .ToList();
+            Console.WriteLine("Lista de artistas ordenados");
+            foreach(var artista in artistasOrdenados)
+            {
+                Console.WriteLine($"- {artista}");
+            }
+        }
+    }
+}
